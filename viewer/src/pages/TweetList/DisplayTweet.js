709c3374a5fc6109dd7e740c4cc5b98c502f569b
fuga takata <maple1213sof@i.softbank.jp>
2019-06-17 16:06:52 +0900

    add JST

--- viewer/src/pages/TweetList/DisplayTweet.js
+++ viewer/src/pages/TweetList/DisplayTweet.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 class DisplayTweet extends React.Component {
     render() {
+        const jst = new Date(this.props.tweet.JSTtime.value)
+        console.log(jst)
         return (
             <div className="media">
                 <div className="media-content">
-                    <div class="content">
+                    <div className="content">
                         <p>
-                            <strong>{this.props.name}</strong>
+                            <strong>{this.props.tweet.name}</strong>
                             <br/>
-                            {this.props.text}
+                            {this.props.tweet.text}
                             <br/>
-                            <small><a>Like</a> · <a>Reply</a>· 3 hrs</small>
+                            <small><a>Like</a> · {`${jst}`}</small>
                         </p>
                     </div>
                 </div>
