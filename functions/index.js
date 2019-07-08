f0db59ebca459acc8cb9014d694d6d30fe72f313
fuga takata <maple1213sof@i.softbank.jp>
2019-07-08 17:12:33 +0900

    add app.get'/details' fuction. it can query with user_id

--- functions/index.js
+++ functions/index.js
@@ -4,30 +4,30 @@ const cors = require('cors')
 const { BigQuery } = require('@google-cloud/bigquery')
 
 const requestQuery = (query, params) => {
-  const bigquery = new BigQuery({
-    projectId: 'moe-twitter-analysis2019'
-  })
-  return bigquery.query({
-    query,
-    params,
-    location: 'asia-northeast1',
-    useLegacySql: false
-  })
+	const bigquery = new BigQuery({
+		projectId: 'moe-twitter-analysis2019'
+	})
+	return bigquery.query({
+		query,
+		params,
+		location: 'asia-northeast1',
+		useLegacySql: false
+	})
 }
 
-const app = express();
+const app = express()
 app.use(cors({ origin: true }))
 
-app.get('/tweets', function (req, res) {
-  if (req.query.keyword === undefined) {
-    res.status(400).send('No keyword defined!')
-  }else{
-    const { keyword, offset } = req.query
-    if(req.query.offset == undefined) {
-      params.offset = 0
-    }
-    const params = { keyword: `%${keyword}%`, offset: +offset }
-    const query = `
+app.get('/tweets', function(req, res) {
+	if (req.query.keyword === undefined) {
+		res.status(400).send('No keyword defined!')
+	} else {
+		const { keyword, offset } = req.query
+		if (req.query.offset == undefined) {
+			params.offset = 0
+		}
+		const params = { keyword: `%${keyword}%`, offset: +offset }
+		const query = `
     SELECT
       text,
       user.name,
@@ -45,12 +45,41 @@ app.get('/tweets', function (req, res) {
     OFFSET
       @offset
     `
-    requestQuery(query, params).then(([rows]) => {
-      return res.status(200).send(rows)
-    }).catch((error) => {
-      return res.status(500).send(error)
-    })
-  }
+		requestQuery(query, params)
+			.then(([rows]) => {
+				return res.status(200).send(rows)
+			})
+			.catch(error => {
+				return res.status(500).send(error)
+			})
+	}
+})
+
+app.get('/details', function(req, res) {
+	const { user_id, offset } = req.query
+	const params = { user_id, offset: +offset }
+	const query = `
+  SELECT
+    text,
+    user.name,
+    DATETIME(created_at, 'Asia/Tokyo') as JSTtime
+  FROM
+    \`moe-twitter-analysis2019.PQ.tweets\`
+  WHERE
+    user.id_str = @user_id
+  ORDER BY
+    JSTtime
+  LIMIT
+    1000
+  OFFSET
+    0`
+    requestQuery(query, params)
+      .then(([rows]) => {
+        return res.status(200).send(rows)
+      })
+      .catch(error => {
+        return res.status(500).send(error)
+      })
 })
 
 exports.main = functions.https.onRequest(app)
