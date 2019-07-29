5fbd343d94914df2e70b4667c6b9c11c8e941740
fuga takata <maple1213sof@i.softbank.jp>
2019-07-29 11:23:33 +0900

    add date filter

--- functions/index.js
+++ functions/index.js
@@ -22,31 +22,32 @@ app.get('/tweets', function(req, res) {
 	if (req.query.keyword === undefined) {
 		res.status(400).send('No keyword defined!')
 	} else {
-		const { keyword, offset } = req.query
+		const { keyword, offset, date } = req.query
 		if (req.query.offset === undefined) {
 			params.offset = 0
 		}
 		const params = {
 			keyword: `%${keyword}%`,
 			offset: +offset,
-			date: date.replace('+', ' ')
+			date: new Date(decodeURIComponent(date))
 		}
 		const query = `
-    SELECT
-      text,
-      user,
-      retweeted_status,
-      DATETIME(created_at, 'Asia/Tokyo') as JSTtime
+	SELECT
+		text,
+		user,
+		retweeted_status,
+		created_at,
+		DATETIME(created_at, 'Asia/Tokyo') as JSTtime
     FROM
-      \`moe-twitter-analysis2019.PQ.tweets\`
+		\`moe-twitter-analysis2019.PQ.tweets\`
     WHERE
-      (text LIKE @keyword) AND (JSTtime >= @date AND JSTtime < DATETIME_ADD(DATETIME , @date INTERVAL 1 MONTH))
+		text LIKE @keyword AND DATETIME(@date) <= DATETIME(created_at, 'Asia/Tokyo') AND DATETIME(created_at, 'Asia/Tokyo') < DATETIME_ADD(DATETIME(@date), INTERVAL 1 MONTH)
     ORDER BY
-      JSTtime
+		JSTtime
     LIMIT
-      1000
+		1000
     OFFSET
-      @offset
+		@offset
     `
 		requestQuery(query, params)
 			.then(([rows]) => {
@@ -62,19 +63,19 @@ app.get('/details', function(req, res) {
 	const { user_id, offset } = req.query
 	const params = { user_id, offset: +offset }
 	const query = `
-  SELECT
+		SELECT
     text,
     user,
     DATETIME(created_at, 'Asia/Tokyo') as JSTtime
-  FROM
+		FROM
     \`moe-twitter-analysis2019.PQ.tweets\`
-  WHERE
+		WHERE
     user.id_str = @user_id
-  ORDER BY
+		ORDER BY
     JSTtime
-  LIMIT
+		LIMIT
     1000
-  OFFSET
+		OFFSET
     @offset`
 	requestQuery(query, params)
 		.then(([rows]) => {
