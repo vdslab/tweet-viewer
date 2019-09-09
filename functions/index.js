bbefc7950b9a6fd78c9f12c756c461f5f7d33e74
fuga takata <maple1213sof@i.softbank.jp>
2019-09-09 12:09:39 +0900

    add condition params use params as a array

--- functions/index.js
+++ functions/index.js
@@ -4,45 +4,50 @@ const cors = require('cors')
 const { BigQuery } = require('@google-cloud/bigquery')
 
 const requestQuery = (query, params) => {
-	const bigquery = new BigQuery({
-		projectId: 'moe-twitter-analysis2019'
-	})
-	return bigquery.query({
-		query,
-		params,
-		location: 'asia-northeast1',
-		useLegacySql: false
-	})
+  const bigquery = new BigQuery({
+    projectId: 'moe-twitter-analysis2019'
+  })
+  return bigquery.query({
+    query,
+    params,
+    location: 'asia-northeast1',
+    useLegacySql: false
+  })
 }
 
 const app = express()
 app.use(cors({ origin: true }))
 
 app.get('/tweets', function(req, res) {
-	if (req.query.keyword === undefined) {
-		res.status(400).send('No keyword defined!')
-	} else {
-		const { keyword, offset, date } = req.query
-		if (req.query.offset === undefined) {
-			params.offset = 0
-		}
-		let start_date;
-		let end_date;
-		if(date) {
-			start_date = new Date(decodeURIComponent(date))
-			end_date = new Date(decodeURIComponent(date))
-			end_date.setMonth(end_date.getMonth() + 1)
-		}else{
-			start_date = new Date("1970-01-01T00:00:00")
-			end_date = new Date("2100-01-01T00:00:00")
-		}
-		const params = {
-			keyword: `%${keyword}%`,
-			offset: +offset,
-			start_date,
-			end_date
-		}
-		const query = `
+  const conditions = []
+  const params = []
+  const { keywords, offset, date, exRt } = req.query
+  let startDate
+  let endDate
+  if (date) {
+    startDate = new Date(decodeURIComponent(date))
+    endDate = new Date(decodeURIComponent(date))
+    endDate.setMonth(endDate.getMonth() + 1)
+  } else {
+    startDate = new Date('1970-01-01T00:00:00')
+    endDate = new Date('2100-01-01T00:00:00')
+  }
+  params.push(startDate, endDate)
+  conditions.push(
+    "DATETIME(?) <= DATETIME(created_at, 'Asia/Tokyo')",
+    "DATETIME(created_at, 'Asia/Tokyo') < DATETIME(?)"
+  )
+  if (exRt === 'yes') {
+    conditions.push('retweeted_status is NULL')
+  }
+  decodeURIComponent(keywords)
+    .split(' ')
+    .map((r) => conditions.push(`text LIKE '%${r}%'`))
+  if (req.query.offset === undefined) {
+    params.offset = 0
+  }
+  params.push(+offset)
+  const query = `
 	SELECT
 		text,
 		user,
@@ -51,77 +56,80 @@ app.get('/tweets', function(req, res) {
 		DATETIME(created_at, 'Asia/Tokyo') as JSTtime
     FROM
 		\`moe-twitter-analysis2019.PQ.tweets\`
-    WHERE
-		text LIKE @keyword AND DATETIME(@start_date) <= DATETIME(created_at, 'Asia/Tokyo') AND DATETIME(created_at, 'Asia/Tokyo') < DATETIME(@end_date)
+    ${conditions.length !== 0 ? 'WHERE' : ''}
+		${conditions.join(' AND ')}
     ORDER BY
 		JSTtime
     LIMIT
 		1000
     OFFSET
-		@offset
-	`
-		requestQuery(query, params)
-			.then(([rows]) => {
-				return res.status(200).send(rows)
-			})
-			.catch(error => {
-				return res.status(500).send(error)
-			})
-	}
+		?
+  `
+  requestQuery(query, params)
+    .then(([rows]) => {
+      return res.status(200).send(rows)
+    })
+    .catch((error) => {
+      return res.status(500).send(error)
+    })
 })
 
 app.get('/details', function(req, res) {
-	const { user_id, offset } = req.query
-	const params = { user_id, offset: +offset }
-	const query = `
+  const conditions = []
+  const { userId, offset } = req.query
+  const params = { userId, offset: +offset }
+  conditions.push('user.id_str = @userId')
+  const query = `
 		SELECT
     text,
     user,
     DATETIME(created_at, 'Asia/Tokyo') as JSTtime
 		FROM
     \`moe-twitter-analysis2019.PQ.tweets\`
-		WHERE
-    user.id_str = @user_id
+		${conditions.length !== 0 ? 'WHERE' : ''}
+		${conditions.join(' AND ')}
 		ORDER BY
     JSTtime
 		LIMIT
     1000
 		OFFSET
     @offset`
-	requestQuery(query, params)
-		.then(([rows]) => {
-			return res.status(200).send(rows)
-		})
-		.catch(error => {
-			return res.status(500).send(error)
-		})
+  requestQuery(query, params)
+    .then(([rows]) => {
+      return res.status(200).send(rows)
+    })
+    .catch((error) => {
+      return res.status(500).send(error)
+    })
 })
 
 app.get('/retweeted_ranking', function(req, res) {
-	const { offset } = req.query
-	const params = { offset: +offset }
-	const query = `
+  const conditions = []
+  const { offset } = req.query
+  const params = { offset: +offset }
+  conditions.push('retweeted_status IS NOT NULL')
+  const query = `
 		SELECT
 			retweeted_status.user.id_str,
 			COUNT(*) as count,
 			ANY_VALUE(entities.user_mentions[OFFSET(0)]) as e
 		FROM
 			\`moe-twitter-analysis2019.PQ.tweets\`
-		WHERE
-			retweeted_status IS NOT NULL
+    ${conditions.length !== 0 ? 'WHERE' : ''}
+    ${conditions.join(' AND ')}
 		GROUP BY
 			retweeted_status.user.id_str
 		ORDER BY COUNT(*) DESC
 		LIMIT 1000
 		OFFSET @offset
 	`
-	requestQuery(query, params)
-		.then(([rows]) => {
-			return res.status(200).send(rows)
-		})
-		.catch(error => {
-			return res.status(500).send(error)
-		})
+  requestQuery(query, params)
+    .then(([rows]) => {
+      return res.status(200).send(rows)
+    })
+    .catch((error) => {
+      return res.status(500).send(error)
+    })
 })
 
 exports.main = functions.https.onRequest(app)
