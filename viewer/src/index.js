98926814987d635c4f7a9b67330ed75a34ca1ebe
fuga takata <maple1213sof@i.softbank.jp>
2019-09-23 15:56:22 +0900

    minor adjustments

--- viewer/src/index.js
+++ viewer/src/index.js
@@ -8,28 +8,26 @@ import RetweetedRanking from './pages/RetweetedRanking/index'
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <section className='section columns'>
-          <div className='column is-2'>
-            <div className='box sticky'>
-              <aside className='munu'>
-                <p className='menu-label'>General</p>
-                <ul className='menu-list'>
-                  <li>
-                    <Link to={'/'}>ツイート検索</Link>
-                  </li>
-                  <li>
-                    <Link to={'/retweeted_ranking'}>ランキング</Link>
-                  </li>
-                </ul>
-              </aside>
-            </div>
+      <section className='section columns'>
+        <div className='column is-2'>
+          <div className='box sticky'>
+            <aside className='munu'>
+              <p className='menu-label'>General</p>
+              <ul className='menu-list'>
+                <li>
+                  <Link to={'/'}>ツイート検索</Link>
+                </li>
+                <li>
+                  <Link to={'/retweeted_ranking'}>ランキング</Link>
+                </li>
+              </ul>
+            </aside>
           </div>
-        </section>
+        </div>
         <Route exact path='/' component={TweetList} />
         <Route path='/user/:userId' component={UserDetails} />
         <Route path='/retweeted_ranking' component={RetweetedRanking} />
-      </div>
+      </section>
     </BrowserRouter>
   )
 }
