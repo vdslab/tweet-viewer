d7b4122a7fb77af864ff51f9b112a469113f7221
FugaTakata <maple1213goo@gmail.com>
2019-12-16 18:50:16 +0900

    convert to react hooks

--- viewer/src/pages/Display/DisplayURLRanking.js
+++ viewer/src/pages/Display/DisplayURLRanking.js
@@ -1,29 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-class DisplayURLRanking extends React.Component {
-  render() {
-    return (
-      <div className='media'>
-        <div className='media-content'>
-          <div className='content'>
-            <p>
-              <strong>
-                <Link
-                  to={'/url/' + `${encodeURIComponent(this.props.url.URL)}`}
-                >
-                  {this.props.url.URL}
-                </Link>
-              </strong>
-              <br />
-              <small>このURLが貼られた回数{this.props.url.count}</small>
-              <br />
-            </p>
-          </div>
+export default (props) => {
+  return (
+    <div className='media'>
+      <div className='media-content'>
+        <div className='content'>
+          <p>
+            <strong>
+              <Link to={'/url/' + `${encodeURIComponent(props.url.URL)}`}>
+                {props.url.URL}
+              </Link>
+            </strong>
+            <br />
+            <small>このURLが貼られた回数{props.url.count}</small>
+            <br />
+          </p>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default DisplayURLRanking
