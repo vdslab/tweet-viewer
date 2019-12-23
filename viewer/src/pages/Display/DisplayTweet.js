5f7cf98b79611170618959274d8b01f72f49436e
FugaTakata <maple1213goo@gmail.com>
2019-12-23 16:28:59 +0900

    use hooks

--- viewer/src/pages/Display/DisplayTweet.js
+++ viewer/src/pages/Display/DisplayTweet.js
@@ -1,29 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-class DisplayTweet extends React.Component {
-  render() {
-    const jst = new Date(this.props.tweet.JSTtime.value)
-    return (
-      <div className='media'>
-        <div className='media-content'>
-          <div className='content'>
-            <p>
-              <strong>
-                <Link to={'/user/' + `${this.props.tweet.user.id_str}`}>
-                  {this.props.tweet.user.name}
-                </Link>
-              </strong>
-              <br />
-              {this.props.tweet.text}
-              <br />
-              <small>{`${jst}`}</small>
-            </p>
-          </div>
+const DisplayTweet = (props) => {
+  const jst = new Date(props.tweet.JSTtime.value)
+  return (
+    <div className='media'>
+      <div className='media-content'>
+        <div className='content'>
+          <p>
+            <strong>
+              <Link to={'/user/' + `${props.tweet.user.id_str}`}>
+                {props.tweet.user.name}
+              </Link>
+            </strong>
+            <br />
+            {props.tweet.text}
+            <br />
+            <small>{`${jst}`}</small>
+          </p>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default DisplayTweet
